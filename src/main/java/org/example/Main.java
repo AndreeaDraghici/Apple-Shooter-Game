440e646811c3a4b6d225f2aeb941cbbc18e8fcc3
Clara Bondrescu <72797657+Clara2605@users.noreply.github.com>
2024-01-27 23:16:58 +0200

    split code in classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,12 @@ package org.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Shooter Game");
+        SampleShooterGame gamePanel = new SampleShooterGame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(gamePanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
